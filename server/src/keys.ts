cd49a62e8bc8810a4a45368d284f611c872fbef8
Arthur <arthur.gamblin.dev@gmail.com>
2019-02-20 21:14:57 +0100

    feat(s3): added configuration for S3

--- server/src/keys.ts
+++ server/src/keys.ts
@@ -3,3 +3,6 @@ export const sqlUser = process.env.RDS_USERNAME;
 export const sqlPassword = process.env.RDS_PASSWORD;
 export const sqlDb = process.env.RDS_DB_NAME;
 export const sqlPort: number = parseInt(process.env.RDS_PORT);
+
+export const s3accessKeyId = process.env.S3_ACCESS_KEY_ID;
+export const s3secretKey = process.env.S3_SECRET_KEY;
