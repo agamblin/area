ab8ab2577dd5990e8b234a83e953d24182d3dc37
Arthur <arthur.gamblin.dev@gmail.com>
2019-02-20 23:25:39 +0100

    prod debug sql

--- server/src/server.ts
+++ server/src/server.ts
@@ -2,6 +2,9 @@ import * as express from 'express';
 import * as cors from 'cors';
 import * as bodyParser from 'body-parser';
 
+// UTILS
+import sequelize from './utils/database';
+
 const app = express();
 
 app.use(cors());
@@ -13,3 +16,6 @@ app.get('/', (req: any, res: any) => {
 });
 
 app.listen(8080, () => 'listening on port 8080');
+sequelize.sync({ force: true }).then(() => {
+	app.listen(8080, () => 'listening on port 8080');
+});
