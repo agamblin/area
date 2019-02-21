a37b41305aa5ba62178d576a2f0055106c7cc1a0
Arthur <arthur.gamblin.dev@gmail.com>
2019-02-21 02:12:21 +0100

    DEBUG SERVER

--- server/src/server.ts
+++ server/src/server.ts
@@ -6,7 +6,7 @@ import * as bodyParser from 'body-parser';
 import sequelize from './utils/database';
 
 // ROUTES
-// import authRoutes from './routes/authRoutes';
+import authRoutes from './routes/authRoutes';
 // import userRoutes from './routes/userRoutes';
 
 // // MIDDLEWARE
@@ -22,25 +22,25 @@ app.get('/', (req: any, res: any) => {
 	res.send('hello');
 });
 
-// app.use('/auth', authRoutes);
+app.use('/auth', authRoutes);
 // app.use('/user', requireAuth, userRoutes);
 
-// app.use(
-// 	(
-// 		error: any,
-// 		req: express.Request,
-// 		res: express.Response,
-// 		next: express.NextFunction
-// 	) => {
-// 		req;
-// 		next;
-// 		console.log(error);
-// 		const status = error.statusCode || 500;
-// 		const message = error.message;
-// 		const data = error.data;
-// 		res.status(status).json({ message, data });
-// 	}
-// );
+app.use(
+	(
+		error: any,
+		req: express.Request,
+		res: express.Response,
+		next: express.NextFunction
+	) => {
+		req;
+		next;
+		console.log(error);
+		const status = error.statusCode || 500;
+		const message = error.message;
+		const data = error.data;
+		res.status(status).json({ message, data });
+	}
+);
 
 sequelize.sync().then(() => {
 	app.listen(8080, () => 'listening on port 8080');
