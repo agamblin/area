e318956036e614fcfe913ff503bab11d932707cc
Arthur <arthur.gamblin.dev@gmail.com>
2019-02-24 18:36:48 +0100

    refacto(signup-test): failed test suite

--- client/src/components/auth/__tests__/Signin.spec.tsx
+++ client/src/components/auth/__tests__/Signin.spec.tsx
@@ -1,45 +1,41 @@
-import React from "react";
-import { mount, ReactWrapper } from "enzyme";
-import Signin from "../Signin";
+import React from 'react';
+import { mount, ReactWrapper } from 'enzyme';
+import Signin from '../Signin';
 
-import Root from "../../../Root";
+import Root from '../../../Root';
 
 let signin: ReactWrapper;
 
 beforeEach(() => {
-  signin = mount(
-    <Root>
-      <Signin />
-    </Root>
-  );
+	signin = mount(
+		<Root>
+			<Signin />
+		</Root>
+	);
 });
 
 // signin check
 
-it("contains a Sign in title", () => {
-  expect(signin.find(".content").text()).toEqual("Sign in");
+it('contains a Sign in title', () => {
+	expect(signin.find('.content').text()).toEqual('Sign in');
 });
 
-it("contains a Sign in button", () => {
-  expect(signin.find(".ui.fluid.positive.button").text()).toEqual("Sign in");
+it('contains a Sign in button', () => {
+	expect(signin.find('.ui.fluid.positive.button').text()).toEqual('Sign in');
 });
 
-it("contains a button Sign up", () => {
-  expect(
-    signin
-      .find(".ui.black.basic.button")
-      .first()
-      .text()
-  ).toEqual("Sign up");
-});
-
-it("contains an email field", () => {
-  expect(signin.find("input").first());
-});
-
-it('contains a field email', () => {
+it('contains a button Sign up', () => {
 	expect(
-		wrapped
+		signin
+			.find('.ui.black.basic.button')
+			.first()
+			.text()
+	).toEqual('Sign up');
+});
+
+it('contains an email field', () => {
+	expect(
+		signin
 			.find('input')
 			.first()
 			.prop('name')
@@ -48,7 +44,7 @@ it('contains a field email', () => {
 
 it('contains a password field', () => {
 	expect(
-		wrapped
+		signin
 			.find('input')
 			.last()
 			.prop('name')
@@ -56,5 +52,5 @@ it('contains a password field', () => {
 });
 
 afterEach(() => {
-  signin.unmount();
+	signin.unmount();
 });
