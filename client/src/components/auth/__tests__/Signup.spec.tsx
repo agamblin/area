b7fc815d61cc3aa3b43f29ef9a6e6e1ed5af4bb0
Biggy <sebastien.assali@epitech.eu>
2019-02-25 19:01:30 +0100

    tu (fixed approximative check): signup

--- client/src/components/auth/__tests__/Signup.spec.tsx
+++ client/src/components/auth/__tests__/Signup.spec.tsx
@@ -1,48 +1,55 @@
-import React from 'react';
-import { mount, ReactWrapper } from 'enzyme';
-import Signup from '../Signup';
+import React from "react";
+import { mount, ReactWrapper } from "enzyme";
+import Signup from "../Signup";
 
-import Root from '../../../Root';
+import Root from "../../../Root";
 
 let signup: ReactWrapper;
 
 beforeEach(() => {
-	signup = mount(
-		<Root>
-			<Signup />
-		</Root>
-	);
+  signup = mount(
+    <Root>
+      <Signup />
+    </Root>
+  );
 });
 
 // signup check
 
-it('contains an button Sign up', () => {
-	expect(
-		signup
-			.find('.ui.positive.button')
-			.first()
-			.text()
-	).toEqual('Sign up');
+it("contains an button Sign up", () => {
+  expect(
+    signup
+      .find(".ui.positive.button")
+      .first()
+      .text()
+  ).toEqual("Sign up");
 });
 
-it('contains an button Cancel', () => {
-	expect(
-		signup
-			.find('.ui.button')
-			.first()
-			.text()
-	).toEqual('Cancel');
+it("contains an button Cancel", () => {
+  expect(
+    signup
+      .find(".ui.button")
+      .first()
+      .text()
+  ).toEqual("Cancel");
 });
 
-it('contains an email field', () => {
-	expect(
-		signup
-			.find('input')
-			.first()
-			.prop('name')
-	).toEqual('email');
+it("contains an email field", () => {
+  expect(signup.find("input").find({ placeholder: "Email" }));
+});
+
+it("contains an user field", () => {
+  expect(signup.find("input").find({ placeholder: "Username" }));
+});
+
+it("contains an password field", () => {
+  expect(signup.find("input").find({ placeholder: "Password" }));
+});
+
+it("contains an password confirm field", () => {
+  expect(signup.find("input").find({ placeholder: "Confirm password" }));
 });
 
 afterEach(() => {
-	signup.unmount();
+  signup.unmount();
 });
