6204bce84315fecfcb70ec4fadf9bc352ca9f393
Arthur <arthur.gamblin.dev@gmail.com>
2019-03-03 14:21:21 +0100

    refacto(services): added header for component services + fixed submit button for signupdetails form

--- client/src/components/auth/ServicesAuth.tsx
+++ client/src/components/auth/ServicesAuth.tsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Grid } from 'semantic-ui-react';
+import { Grid, Header } from 'semantic-ui-react';
 import GoogleLogin from './Services/GoogleLogin';
 import GithubLogin from './Services/GithubLogin';
 import TrelloLogin from './Services/TrelloLogin';
@@ -7,19 +7,27 @@ import TrelloLogin from './Services/TrelloLogin';
 export class ServicesAuth extends Component {
 	render() {
 		return (
-			<Grid columns={3} centered>
-				<Grid.Row>
-					<Grid.Column width={4} textAlign="center">
-						<GoogleLogin />
-					</Grid.Column>
-					<Grid.Column width={4} textAlign="center">
-						<TrelloLogin />
-					</Grid.Column>
-					<Grid.Column width={4} textAlign="center">
-						<GithubLogin />
-					</Grid.Column>
-				</Grid.Row>
-			</Grid>
+			<React.Fragment>
+				<Header
+					as="h1"
+					content="Services"
+					subheader="Connect to Google, Trello and Github to power-up your productivity !"
+					dividing
+				/>
+				<Grid columns={3} centered style={{ marginTop: '2.5%' }}>
+					<Grid.Row>
+						<Grid.Column width={4} textAlign="center">
+							<GoogleLogin />
+						</Grid.Column>
+						<Grid.Column width={4} textAlign="center">
+							<TrelloLogin />
+						</Grid.Column>
+						<Grid.Column width={4} textAlign="center">
+							<GithubLogin />
+						</Grid.Column>
+					</Grid.Row>
+				</Grid>
+			</React.Fragment>
 		);
 	}
 }
